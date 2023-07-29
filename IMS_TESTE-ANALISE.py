c2bd96bcee65f6a2370a5d6c40a9e2152197fc8d
Felipe <felipe.1993.mw@gmail.com>
2023-07-28 21:24:43 -0300

    Primeiro Commit

--- IMS_TESTE-ANALISE.py
+++ IMS_TESTE-ANALISE.py
@@ -2,17 +2,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy import interpolate
 import seaborn as sns
 
-df = pd.read_excel(r"C:\Users\fmour\Documents\MEGAsync Downloads\Documentos\Mestrado\DIAGNOSTICOS.xlsx")
+df = pd.read_excel(r"C:\Users\Felipe\DIAGNOSTICOS.xlsx")
 
-df['Dia'] = pd.to_datetime(df['Registro']).dt.day_name()
-
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_colwidth', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_rows', None)
+df['Dia'] = df['Registro'].dt.day
 
 print(df)
 
@@ -35,14 +29,7 @@ ax1.set_ylabel('Voltagem')
 # GRF 2 - BOXPLOT NO PERIODO TOTAL #
 ax2 = sns.catplot(data=df, x="Dia", y='Vr [V]', kind="box")
 
-# GRF 3 - TENSÃO DIÁRIA #
-filt = (df['Registro'].dt.day == 2)
-sexta = df.loc[filt]
-sexta['Timestamp'] = pd.to_datetime(sexta['Registro']).values.astype(np.int64)//10*9
-
-g = interpolate.interp1d(sexta['Timestamp'], sexta['Vr [V]'], kind='nearest')
-ynew = g(sexta['Timestamp'])
-fig2, ax3 = plt.subplots(figsize=(10,5), label='Tensão Diária')
-ax3.plot(sexta['Timestamp'],ynew)
+fig, ax2 = plot.subplot(figsize=(7,5), label='Tensões Diárias')
+ax2.plot
 
 plt.show()
